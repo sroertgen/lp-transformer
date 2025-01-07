8844d81b403a11e30cbfde9e7d423e34b98ae459
@s.roertgen <sroertgen@gmail.com>
2025-01-07 16:45:09 +0100

    Add Link Icon

--- src/lib/components/AddLink.svelte
+++ src/lib/components/AddLink.svelte
@@ -1,12 +1,12 @@
 <script>
-	import Plus from '$lib/components/icons/Plus.svelte';
+	import Link from '$lib/components/icons/Link.svelte';
 
 	import { db, addFragment, updateNode } from '$lib/db';
 	import config from '$lib/config';
 </script>
 
 <details class="dropdown">
-	<summary class="btn m-1"> <Plus /> </summary>
+	<summary class="btn m-1"> <Link /> </summary>
 	<ul class="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow">
 		{#each Object.keys(config[$db.selectedState]) as bereich}
 			{@const bereichNodes = $db.nodes.filter((n) => n.type === bereich)}
